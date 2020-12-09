7af9adbc33442120930bd8b6990c28d66fcd4513
dblinn <david.blinn@turbonomic.com>
2020-12-09 09:52:31 -0500

    Improve day 9 solution by looking at a better solution

--- src/bin/day_09.rs
+++ src/bin/day_09.rs
@@ -28,29 +28,25 @@ fn has_two_sum(valid_values: &HashSet<usize>, num: usize) -> bool {
         .any(|v| num > *v && num - v != *v && valid_values.contains(&(num - v)))
 }
 
-fn find_invalid_range(input: &str, invalid_num: usize, invalid_index: usize) -> Option<Vec<usize>> {
+fn find_invalid_range(input: &str, invalid_num: usize) -> usize {
     let possible = input.lines()
         .map(|v|v.parse::<usize>().expect(&format!("A valid number. Not {}", v)))
         .collect::<Vec<usize>>();
 
-    println!("Attempting to find invalid_num {} with invalid_index {}", invalid_num, invalid_index);
-    for (i, val) in possible[..invalid_index].iter().enumerate() {
-        let mut sum = *val;
-        let mut values: Vec<usize> = vec!(*val);
-        for next in possible[i+1..invalid_index].iter() {
-            sum += *next;
-            values.push(*next);
-            if sum >= invalid_num {
-                break;
-            }
-        }
-
-        if sum == invalid_num {
-            return Some(values);
+    let (mut i, mut j, mut sum) = (0, 0, 0);
+    while sum != invalid_num {
+        if sum < invalid_num {
+            sum += possible[j];
+            j += 1;
+        } else if sum > invalid_num {
+            sum -= possible[i];
+            i += 1;
+        } else {
+            break;
         }
     }
 
-    None
+    possible[i..j].iter().min().unwrap () + possible[i..j].iter().max().unwrap()
 }
 
 fn part1(input: &str) -> usize {
@@ -59,11 +55,7 @@ fn part1(input: &str) -> usize {
 
 fn part2(input: &str, preamble_length: usize) -> usize {
     if let Some((invalid_num, invalid_index)) = find_invalid(input,  preamble_length) {
-        let mut invalid_range: Vec<usize> = find_invalid_range(input, invalid_num, invalid_index)
-            .expect("has invalid range");
-        println!("Invalid range: {:?}", invalid_range);
-        invalid_range.sort();
-        invalid_range.first().unwrap() + invalid_range.last().unwrap()
+        find_invalid_range(input, invalid_num)
     } else {
         panic!("Expected an invalid number");
     }
