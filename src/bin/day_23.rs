583d664b30634f60b1f760c9f8b355df028bb92d
dblinn <david.blinn@turbonomic.com>
2020-12-27 16:20:26 -0500

    Faster solution using fancy Vec operations like drain and splice. Would have taken about an hour in release mode to get the answer

--- src/bin/day_23.rs
+++ src/bin/day_23.rs
@@ -1,16 +1,23 @@
-#![feature(linked_list_cursors)]
-
 use std::error::Error;
-use std::collections::LinkedList;
+use std::ops::Index;
+use itertools::Itertools;
 
 #[derive(Debug)]
 struct Circle {
-    cups: LinkedList<usize>,
+    cups: Vec<usize>,
     cup_count: usize
 }
 
+impl Index<usize> for Circle {
+    type Output = usize;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.cups[index % self.cups.len()]
+    }
+}
+
 impl Circle {
-    fn new(cups: LinkedList<usize>) -> Self {
+    fn new(cups: Vec<usize>) -> Self {
         Self { cup_count: cups.len(), cups: cups }
     }
 
@@ -29,27 +36,18 @@ impl Circle {
     }
 
     fn play_round(&mut self) {
-        let current = self.cups.pop_front().unwrap();
-        let pick_ups = (self.cups.pop_front().unwrap(),
-                        self.cups.pop_front().unwrap(),
-                        self.cups.pop_front().unwrap());
+        let current = self.cups[0];
+        let pick_ups = (self.cups[1], self.cups[2], self.cups[3]);
 
-        self.cups.push_back(current);
+        self.cups.push(current);
+        self.cups.drain(0..4);
 
         let destination = self.find_destination(current, &pick_ups);
-        let mut cursor = self.cups.cursor_front_mut();
-        loop {
-            if let Some(&mut val) = cursor.current() {
-                if val == destination {
-                    break;
-                }
-            }
-            cursor.move_next();
-        }
-
-        cursor.insert_after(pick_ups.2);
-        cursor.insert_after(pick_ups.1);
-        cursor.insert_after(pick_ups.0);
+        let (index, _) = self.cups.iter()
+            .find_position(|&&cup| cup == destination)
+            .unwrap();
+        self.cups.splice((index + 1) .. (index + 1),
+                         [pick_ups.0, pick_ups.1, pick_ups.2].iter().cloned());
     }
 }
 
@@ -60,26 +58,18 @@ fn part1(input: &str) -> usize {
         circle.play_round();
     }
 
-    let mut answer = String::with_capacity(circle.cup_count);
-    let mut cursor = circle.cups.cursor_front();
-    while cursor.current() != Some(&1) {
-        cursor.move_next();
-    }
-    for _ in 0 .. circle.cup_count {
-        cursor.move_next();
-        if let Some(&val) = cursor.current() {
-            answer.push((val as u8 + b'0') as char);
-        }
-    }
-
-    answer.parse::<usize>().unwrap()
+    let index1 = circle.cups.iter().find_position(|&&cup| cup == 1).unwrap().0;
+    let val = (index1 + 1 .. index1 + circle.cup_count).into_iter()
+        .map(|i| (circle[i] as u8 + b'0') as char)
+        .collect::<String>();
+    val.parse::<usize>().unwrap()
 }
 
 fn part2(input: &str) -> usize {
     let mut circle = Circle::new(input.chars()
         .map(|c| c.to_digit(10).unwrap() as usize).collect());
     for i in circle.cup_count .. 1_000_000 {
-        circle.cups.push_back(i);
+        circle.cups.push(i);
     }
 
     for i in 0 .. 10_000_000 {
@@ -89,15 +79,9 @@ fn part2(input: &str) -> usize {
         circle.play_round();
     }
 
-    let mut cursor = circle.cups.cursor_front();
-    while cursor.current() != Some(&1) {
-        cursor.move_next();
-    }
-
-    cursor.move_next();
-    let a = *cursor.current().unwrap();
-    cursor.move_next();
-    let b = *cursor.current().unwrap();
+    let index1 = circle.cups.iter().find_position(|&&cup| cup == 1).unwrap().0;
+    let a = circle[index1 + 1];
+    let b = circle[index1 + 2];
     a * b
 }
 
